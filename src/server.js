252053aac71b5333cb77f5153e85f7753b05a26f
Blockost <simon.espigole@gmail.com>
2019-01-20 18:16:15 -0500

    Embedded app into a node server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "static")));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", {root: 'src/static/'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}...`);
+});
