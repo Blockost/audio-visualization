72086f54420f2ed98853148368da9bc75ff76d51
Blockost <simon.espigole@gmail.com>
2019-01-20 19:56:28 -0500

    Moved app's entry point (server.js) at root location

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "static")));
+app.use("/", express.static(path.join(__dirname, "src/static")));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", {root: 'src/static/'});
